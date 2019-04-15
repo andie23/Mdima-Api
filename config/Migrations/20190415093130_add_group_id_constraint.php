5f5733e05af3d4ddf6bfab5e34c433b2139bbccf
Andrew Mfune <andiemfune@gmail.com>
2019-04-15 12:03:22 +0200

    Added foreign key constraints to some tables

---
+++ config/Migrations/20190415093130_add_group_id_constraint.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddGroupIdConstraint extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->query("ALTER TABLE `schedules` ADD CONSTRAINT `group_id` FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;");
+    }
+}
---
+++ config/Migrations/20190415093848_allocation_area_id_constraint.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AllocationAreaIdConstraint extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->query("ALTER TABLE `allocations` ADD CONSTRAINT `allocation_area_id` FOREIGN KEY (`area_id`) REFERENCES `areas`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;");
+    }
+}
